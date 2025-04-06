2eb61b0ce672c0f6b6c09738ffaf806bc06d1574
Nycolas <nycolas@teste.com>
2025-04-06 10:38:10 -0300

    site completo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
 
 function images(){
     return gulp.src('./src/images/**/*')
@@ -9,4 +8,13 @@ function images(){
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(images);
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compreessed'}))
+    .pipe(gulp.dest('./dist/styles'));
+}
+
+exports.default = gulp.parallel(styles, images);
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+};
